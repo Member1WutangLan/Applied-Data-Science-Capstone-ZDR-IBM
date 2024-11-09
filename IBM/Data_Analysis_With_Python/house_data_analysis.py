04f3d5bb9d3b152e4d2c8cba370d1577441bd171
Zack Ruiz <105403053+Member1WutangLan@users.noreply.github.com>
2024-11-09 10:03:26 -0700

    Add files via upload

---
+++ IBM/Data_Analysis_With_Python/house_data_analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+
+# Load the CSV file from the URL
+file_name = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/FinalModule_Coursera/data/kc_house_data_NaN.csv'
+df = pd.read_csv(file_name)
+
+# Display the data types of each column
+print(df.dtypes)
